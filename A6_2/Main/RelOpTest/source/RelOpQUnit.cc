9ec848a85772ff8592db47d47c131483396766be
jx17 <junruxia@hotmail.com>
2017-04-02 16:42:25 -0500

    Revert "df"
    This reverts commit 1837ca902dcfa93d706ee3600590136b294aa3b4.

--- A6_2/Main/RelOpTest/source/RelOpQUnit.cc
+++ A6_2/Main/RelOpTest/source/RelOpQUnit.cc
@@ -29,90 +29,100 @@ using namespace std;
 int main () {
     
     QUnit::UnitTest qunit(cerr, QUnit::verbose);
-     {
-        // create a catalog
-        MyDB_CatalogPtr myCatalog = make_shared <MyDB_Catalog> ("catFile");
-        
-        // now make a schema
-        MyDB_SchemaPtr mySchemaL = make_shared <MyDB_Schema> ();
-        mySchemaL->appendAtt (make_pair ("l_suppkey", make_shared <MyDB_IntAttType> ()));
-        mySchemaL->appendAtt (make_pair ("l_name", make_shared <MyDB_StringAttType> ()));
-        mySchemaL->appendAtt (make_pair ("l_address", make_shared <MyDB_StringAttType> ()));
-        mySchemaL->appendAtt (make_pair ("l_nationkey", make_shared <MyDB_IntAttType> ()));
-        mySchemaL->appendAtt (make_pair ("l_phone", make_shared <MyDB_StringAttType> ()));
-        mySchemaL->appendAtt (make_pair ("l_acctbal", make_shared <MyDB_DoubleAttType> ()));
-        mySchemaL->appendAtt (make_pair ("l_comment", make_shared <MyDB_StringAttType> ()));
-        
-        // and a right schema
-        MyDB_SchemaPtr mySchemaR = make_shared <MyDB_Schema> ();
-        mySchemaR->appendAtt (make_pair ("r_suppkey", make_shared <MyDB_IntAttType> ()));
-        mySchemaR->appendAtt (make_pair ("r_name", make_shared <MyDB_StringAttType> ()));
-        mySchemaR->appendAtt (make_pair ("r_address", make_shared <MyDB_StringAttType> ()));
-        mySchemaR->appendAtt (make_pair ("r_nationkey", make_shared <MyDB_IntAttType> ()));
-        mySchemaR->appendAtt (make_pair ("r_phone", make_shared <MyDB_StringAttType> ()));
-        mySchemaR->appendAtt (make_pair ("r_acctbal", make_shared <MyDB_DoubleAttType> ()));
-        mySchemaR->appendAtt (make_pair ("r_comment", make_shared <MyDB_StringAttType> ()));
-        
+    
+    // create a catalog
+    MyDB_CatalogPtr myCatalog = make_shared <MyDB_Catalog> ("catFile");
+    
+    // now make a schema
+    MyDB_SchemaPtr mySchemaL = make_shared <MyDB_Schema> ();
+    mySchemaL->appendAtt (make_pair ("l_suppkey", make_shared <MyDB_IntAttType> ()));
+    mySchemaL->appendAtt (make_pair ("l_name", make_shared <MyDB_StringAttType> ()));
+    mySchemaL->appendAtt (make_pair ("l_address", make_shared <MyDB_StringAttType> ()));
+    mySchemaL->appendAtt (make_pair ("l_nationkey", make_shared <MyDB_IntAttType> ()));
+    mySchemaL->appendAtt (make_pair ("l_phone", make_shared <MyDB_StringAttType> ()));
+    mySchemaL->appendAtt (make_pair ("l_acctbal", make_shared <MyDB_DoubleAttType> ()));
+    mySchemaL->appendAtt (make_pair ("l_comment", make_shared <MyDB_StringAttType> ()));
+    
+    // and a right schema
+    MyDB_SchemaPtr mySchemaR = make_shared <MyDB_Schema> ();
+    mySchemaR->appendAtt (make_pair ("r_suppkey", make_shared <MyDB_IntAttType> ()));
+    mySchemaR->appendAtt (make_pair ("r_name", make_shared <MyDB_StringAttType> ()));
+    mySchemaR->appendAtt (make_pair ("r_address", make_shared <MyDB_StringAttType> ()));
+    mySchemaR->appendAtt (make_pair ("r_nationkey", make_shared <MyDB_IntAttType> ()));
+    mySchemaR->appendAtt (make_pair ("r_phone", make_shared <MyDB_StringAttType> ()));
+    mySchemaR->appendAtt (make_pair ("r_acctbal", make_shared <MyDB_DoubleAttType> ()));
+    mySchemaR->appendAtt (make_pair ("r_comment", make_shared <MyDB_StringAttType> ()));
+    
+    // use the schema to create a table
+    MyDB_TablePtr myTableLeft = make_shared <MyDB_Table> ("supplierLeft", "supplierLeft.bin", mySchemaL);
+    MyDB_TablePtr myTableRight = make_shared <MyDB_Table> ("supplierRight", "supplierRight.bin", mySchemaR);
+    
+    // get the tables
+    MyDB_BufferManagerPtr myMgr = make_shared <MyDB_BufferManager> (1310720, 256, "tempFile");
+    MyDB_TableReaderWriterPtr supplierTableL = make_shared <MyDB_TableReaderWriter> (myTableLeft, myMgr);
+    MyDB_TableReaderWriterPtr supplierTableRNoBPlus = make_shared <MyDB_TableReaderWriter> (myTableRight, myMgr);
+    
+    // load up from a text file
+    cout << "loading left table.\n";
+    // supplierTableL->loadFromTextFile ("smallSupplier.tbl");
+   	supplierTableL->loadFromTextFile ("/Users/kejunliu/Documents/DataBase-530-2/A6_2/Build/supplier.tbl");
+    
+    cout << "loading right table.\n";
+    supplierTableRNoBPlus->loadFromTextFile ("/Users/kejunliu/Documents/DataBase-530-2/A6_2/Build/supplier.tbl");
+   	// supplierTableRNoBPlus->loadFromTextFile ("supplierBig.tbl");
+    
+    {
+        // get the output schema and table
         MyDB_SchemaPtr mySchemaOut = make_shared <MyDB_Schema> ();
         mySchemaOut->appendAtt (make_pair ("l_name", make_shared <MyDB_StringAttType> ()));
-        mySchemaOut->appendAtt (make_pair ("combined_comment", make_shared <MyDB_StringAttType> ()));
-        
-        // use the schema to create a table
-        MyDB_TablePtr myTableLeft = make_shared <MyDB_Table> ("supplierLeft", "supplierLeft.bin", mySchemaL);
-        MyDB_TablePtr myTableRight = make_shared <MyDB_Table> ("supplierRight", "supplierRight.bin", mySchemaR);
+        mySchemaOut->appendAtt (make_pair ("l_nationkey", make_shared <MyDB_StringAttType> ()));
+        mySchemaOut->appendAtt (make_pair ("combined_stuff", make_shared <MyDB_StringAttType> ()));
         MyDB_TablePtr myTableOut = make_shared <MyDB_Table> ("supplierOut", "supplierOut.bin", mySchemaOut);
-        
-        // get the tables
-        MyDB_BufferManagerPtr myMgr = make_shared <MyDB_BufferManager> (131072, 128, "tempFile");
-        MyDB_TableReaderWriterPtr supplierTableL = make_shared <MyDB_TableReaderWriter> (myTableLeft, myMgr);
-        MyDB_TableReaderWriterPtr supplierTableR = make_shared <MyDB_TableReaderWriter> (myTableRight, myMgr);
         MyDB_TableReaderWriterPtr supplierTableOut = make_shared <MyDB_TableReaderWriter> (myTableOut, myMgr);
-        
-        // load up from a text file
-        cout << "loading left\n";
-        supplierTableL->loadFromTextFile ("/Users/xiajunru/Code/DataBase-530-2/A6_2/Build/supplier.tbl");
-        cout << "loading right\n";
-        supplierTableR->loadFromTextFile ("/Users/xiajunru/Code/DataBase-530-2/A6_2/Build/supplierBig.tbl");
-        
-        // This basically runs:
-        //
-        // SELECT supplierLeft.l_name, supplierLeft.l_comment + " " + supplierRight.r_comment
-        // FROM supplierLeft, supplierRight
-        // WHERE (supplierLeft.l_nationkey = 4 OR
-        //        supplierLeft.l_nationkey = 3) AND
-        //       (supplierRight.r_nationkey = 3) AND
-        //       (supplierLeft.l_suppkey = supplierRight.r_suppkey) AND
-        //       (supplierLeft.l_name = supplierRight.r_name)
-        //
-        // It does this by hashing the smaller table (supplierLeft) on
-        // supplierLeft.l_suppkey and supplierLeft.l_name.  It then scans
-        // supplierRight, probing the hash table for matches
-        
-        vector <pair <string, string>> hashAtts;
-        hashAtts.push_back (make_pair (string ("[l_suppkey]"), string ("[r_suppkey]")));
-        hashAtts.push_back (make_pair (string ("[l_name]"), string ("[r_name]")));
         
         vector <string> projections;
         projections.push_back ("[l_name]");
-        projections.push_back ("+ (+ ([l_comment], string[ ]), [r_comment])");
-               ScanJoin myOp (supplierTableL, supplierTableR, supplierTableOut,
-                       "&& ( == ([l_suppkey], [r_suppkey]), == ([l_name], [r_name]))", projections, hashAtts,
-                       "|| ( == ([l_nationkey], int[3]), == ([l_nationkey], int[4]))",
-                       "== ([r_nationkey], int[3])");
+        projections.push_back ("[l_nationkey]");
+        projections.push_back ("+ (+ (+ ([l_phone], string[ ]), + ([l_acctbal], string[ ])), [l_comment])");
         
-        cout << "running join\n";
+        RegularSelection myOp (supplierTableL, supplierTableOut, "== ([l_nationkey], int[1])", projections);
         myOp.run ();
         
+        cout << "\nRunning selection.";
+        cout << "\nFirst result should be:\n";
+        cout << "Supplier#000000003|1|11-383-516-1199 4192.400000 furiously regular instructions impress slyly! carefu|\n\n";
         MyDB_RecordPtr temp = supplierTableOut->getEmptyRecord ();
         MyDB_RecordIteratorAltPtr myIter = supplierTableOut->getIteratorAlt ();
- 
->>>>>>> Stashed changes
+        
+        while (myIter->advance ()) {
+            myIter->getCurrent (temp);
+            cout << temp << "\n";
+        }
+        
+        // now, we count the total number of records
+        vector <pair <MyDB_AggType, string>> aggsToCompute;
+        aggsToCompute.push_back (make_pair (MyDB_AggType :: cnt, "int[0]"));
+        
+        vector <string> groupings;
+        MyDB_SchemaPtr mySchemaOutAgain  = make_shared <MyDB_Schema> ();
+        mySchemaOutAgain->appendAtt (make_pair ("mycnt", make_shared <MyDB_IntAttType> ()));
+        MyDB_TablePtr aggTable = make_shared <MyDB_Table> ("aggOut", "aggOut.bin", mySchemaOutAgain);
+        MyDB_TableReaderWriterPtr aggTableOut = make_shared <MyDB_TableReaderWriter> (aggTable, myMgr);
+        
+        Aggregate myOpAgain (supplierTableOut, aggTableOut, aggsToCompute, groupings, "bool[true]");
+        cout << "running aggregate\n";
+        myOpAgain.run ();
+        
+        temp = aggTableOut->getEmptyRecord ();
+        myIter = aggTableOut->getIteratorAlt ();
+        
+        cout << "Now we count the records.";
+        cout << "\nThe output should be 413:\n";
         while (myIter->advance ()) {
             myIter->getCurrent (temp);
             cout << temp << "\n";
         }
     }
-<<<<<<< Updated upstream
     
 //    {
 //        // get the output schema and table
@@ -580,8 +590,6 @@ int main () {
 //        }
 //    }
     
-=======
->>>>>>> Stashed changes
 }
 
 //#endif
