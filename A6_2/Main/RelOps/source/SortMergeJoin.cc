59eedb1202abead581d5ec5a67b8be544b7e67c6
liukejun <kejunliu@KejunLius-MacBook-Air.local>
2017-04-04 21:51:59 -0500

    sort phase

--- A6_2/Main/RelOps/source/SortMergeJoin.cc
+++ A6_2/Main/RelOps/source/SortMergeJoin.cc
@@ -29,5 +29,45 @@ SortMergeJoin :: SortMergeJoin (MyDB_TableReaderWriterPtr leftInput, MyDB_TableR
 
 // execute the join
 void SortMergeJoin:: run (){
-    l;trd
+    // get the left input record, and get the various functions over it
+    MyDB_RecordPtr leftInputRec = leftInput->getEmptyRecord ();
+    // get the right input record, and get the various functions over it
+    MyDB_RecordPtr rightInputRec = rightInput->getEmptyRecord ();
+    // and get the schema that results from combining the left and right records
+    MyDB_SchemaPtr mySchemaOut = make_shared <MyDB_Schema> ();
+    for (auto p : leftInput->getTable ()->getSchema ()->getAtts ())
+        mySchemaOut->appendAtt (p);
+    for (auto p : rightInput->getTable ()->getSchema ()->getAtts ())
+        mySchemaOut->appendAtt (p);
+    // get the combined record
+    MyDB_RecordPtr combinedRec = make_shared <MyDB_Record> (mySchemaOut);
+    
+    // and make it a composite of the two input records
+    combinedRec->buildFrom (leftInputRec, rightInputRec);
+    
+    
+    // now, get the final predicate over it
+    func finalPredicate = combinedRec->compileComputation (finalSelectionPredicate);
+    // left: get the various functions whose output we'll hash
+    func leftEqualities;
+    leftEqualities.push_back (leftInputRec->compileComputation (p.first));
+    
+    // right: get the various functions whose output we'll hash
+    func rightEqualities;
+    rightEqualities.push_back (rightInputRec->compileComputation (p.second));
+    
+    //-----------Sort phase--------
+    MyDB_TableReaderWriterPtr leftSorted = make_shared<MyDB_TableReaderWriter>();
+    MyDB_RecordPtr lhsL = leftInput->getEmptyRecord();
+    MyDB_RecordPtr rhsL = leftInput->getEmptyRecord();
+    sort (64, leftInput, leftSorted, leftEqualities, lhsL, rhsL);
+    
+    MyDB_TableReaderWriterPtr rightSorted = make_shared<MyDB_TableReaderWriter>();
+    MyDB_RecordPtr lhsR = leftInput->getEmptyRecord();
+    MyDB_RecordPtr rhsR = leftInput->getEmptyRecord();
+    sort (64, rightInput, rightSorted, rightEqualities, lhsR, rhsR);
+    
+    //---------Merge phase---------
+    
+    
 }
